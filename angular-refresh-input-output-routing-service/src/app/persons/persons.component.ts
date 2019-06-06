f1e2238867db0050d5c25740f10df7d236338aca
John <zero2full@gmail.com>
2019-06-06 07:37:08 -0400

    20160606

--- angular-refresh-input-output-routing-service/src/app/persons/persons.component.ts
+++ angular-refresh-input-output-routing-service/src/app/persons/persons.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  private personListSubscription: Subscription;
 
   constructor(private personsService: PersonsService) {}
 
   ngOnInit() {
     this.personList = this.personsService.persons;
+    this.personListSubscription = this.personsService.personsChanged.subscribe( persons => { this.personList = persons; } );
   }
 
   onRemovePerson(name: string) {
     this.personsService.removePerson(name);
+  }
+
+  ngOnDestroy() {
+    this.personListSubscription.unsubscribe();
   }
 }
